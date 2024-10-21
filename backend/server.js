4b5edf71a42a232ff678224344b0cc5ae9554461
srimant0203 <srimant.acharjya@gmail.com>
2024-10-21 22:53:06 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,11 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
-
+import job from "./cron/cron.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
+job.start();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
